39466a98597e11c167658ed3354036e5efcb7e92
mangeshupadhyay23 <mshanit@gmail.com>
2021-01-31 01:18:55 +0530

    updated frontend

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,13 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Navbar from "./Navbar";
+import * as Mui from '@material-ui/core';
 import { ReactComponent as Home1 } from "../assets/home-1.svg";
+import { ReactComponent as Home2 } from "../assets/diagonal.svg";
+import { ReactComponent as Sf } from "../assets/salesforce.svg";
+import { ReactComponent as Hs } from "../assets/hubspot.svg";
+import { ReactComponent as Pd } from "../assets/pipedrive.svg";
+import './Home.css'
 
 const useStyles = makeStyles((theme) => ({
   header: {
@@ -20,9 +26,18 @@ const useStyles = makeStyles((theme) => ({
   },
   home1: {
     float: "right",
-    marginLeft: "20px",
+    marginLeft: "10px",
     borderRadius: "10px",
   },
+
+  featuresList:{
+    float: 'left',
+    marginTop:"2%",
+    marginBottom: '100px',
+    width: '20%',
+    marginRight: '5%',
+    fontSize:'60%'
+  }
 }));
 
 export default function Home() {
@@ -52,7 +67,7 @@ export default function Home() {
           </button>
         </div>
       </header>
-      <section>
+      <section style={{marginBottom:"100px"}}>
         <div className="row">
           <h2>Wokr With Your Revenue Management Stack</h2>
           <Home1 className={classes.home1} />
@@ -60,7 +75,7 @@ export default function Home() {
             Scale your SaaS through 480+ recurring billing scenarios that
             automate who you bill, when, and how. No humans, no spreadsheets, no
             missed payments!
-            <ul className="features">
+            <ul className={classes.featuresList}>
               <li>
                 <ion-icon name="checkmark-outline"></ion-icon>Billing Schedules
               </li>
@@ -68,6 +83,8 @@ export default function Home() {
                 <ion-icon name="checkmark-outline"></ion-icon>Proration &
                 Invoicing
               </li>
+            </ul>
+            <ul className={classes.featuresList}>
               <li>
                 <ion-icon name="checkmark-outline"></ion-icon>Tax management
               </li>
@@ -76,6 +93,203 @@ export default function Home() {
               </li>
             </ul>
           </p>
+        </div>
+      </section>
+      <section className="digaonal">
+        <div className="row">
+          <h1
+            style={{ textAlign: "justify", color: "white", marginTop: "10px" }}
+          >
+            Works With Your <br />
+            Revenue Management Stack
+          </h1>
+          <br />
+          <p style={{ textAlign: "left", fontSize: "100%", color: "#CDC6DF" }}>
+            Managing sales, payments, customer experience or your books:
+            <br /> if it affects your revenue cycle, Chargebee works with it.
+          </p>
+          <Home2 />
+        </div>
+
+        <div style={{ float: "right" }}>
+          <h3>Power Sales and Marketing CRM</h3>
+          <ul style={{ display: "flex", padding: "2px" }}>
+            <li>
+              <Sf
+                style={{ width: "100px", height: "100px", padding: "10px" }}
+              />
+            </li>
+            <li>
+              <Hs
+                style={{ width: "100px", height: "100px", padding: "10px" }}
+              />
+            </li>
+            <li>
+              <Pd
+                style={{ width: "100px", height: "100px", padding: "10px" }}
+              />
+            </li>
+          </ul>
+          <p>
+            Handle quotes, approvals, acceptance, and
+            <br /> collections seamlessly from within the CRM.
+          </p>
+          <br />
+          <Mui.Button variant="contained" style={{ color: "#000FF0" }}>
+            Learn More&nbsp;
+            <ion-icon
+              name="arrow-forward-outline"
+              style={{ verticalAlign: "middle" }}
+            ></ion-icon>
+          </Mui.Button>
+        </div>
+        <div>
+          <h3>Power Sales and Marketing CRM</h3>
+          <ul style={{ display: "flex", padding: "2px" }}>
+            <li>
+              <Sf
+                style={{ width: "100px", height: "100px", padding: "10px" }}
+              />
+            </li>
+            <li>
+              <Hs
+                style={{ width: "100px", height: "100px", padding: "10px" }}
+              />
+            </li>
+            <li>
+              <Pd
+                style={{ width: "100px", height: "100px", padding: "10px" }}
+              />
+            </li>
+          </ul>
+          <p>
+            Handle quotes, approvals, acceptance, and
+            <br /> collections seamlessly from within the CRM.
+          </p>
+          <br />
+          <Mui.Button variant="contained" style={{ color: "#000FF0" }}>
+            Learn More&nbsp;
+            <ion-icon
+              name="arrow-forward-outline"
+              style={{ verticalAlign: "middle" }}
+            ></ion-icon>
+          </Mui.Button>
+        </div>
+        <div style={{ marginTop: "100px" }}>
+          <h3>Power Sales and Marketing CRM</h3>
+          <ul style={{ display: "flex", padding: "2px" }}>
+            <li>
+              <Sf
+                style={{ width: "100px", height: "100px", padding: "10px" }}
+              />
+            </li>
+            <li>
+              <Hs
+                style={{ width: "100px", height: "100px", padding: "10px" }}
+              />
+            </li>
+            <li>
+              <Pd
+                style={{ width: "100px", height: "100px", padding: "10px" }}
+              />
+            </li>
+          </ul>
+          
+          <p>
+            Handle quotes, approvals, acceptance, and
+            <br /> collections seamlessly from within the CRM.
+          </p>
+          <br />
+          <Mui.Button variant="contained" style={{ color: "#000FF0" }}>
+            Learn More&nbsp;
+            <ion-icon
+              name="arrow-forward-outline"
+              style={{ verticalAlign: "middle" }}
+            ></ion-icon>
+          </Mui.Button>
+        </div>
+
+        <div className="row" style={{ marginTop: "50px" }}>
+          <h3 style={{ textAlign: "center" }}>
+            Talk to us today about your revenue workflow
+          </h3>
+          <Mui.Divider variant="middle" style={{ background: "white" }} />
+          <button className="btn" type="button">
+            <span>
+              <a
+                href="/signup"
+                style={{ textDecoration: "none", color: "white" }}
+              >
+                Schedule a Demo
+                <ion-icon
+                  name="arrow-forward-outline"
+                  style={{ verticalAlign: "middle" }}
+                ></ion-icon>
+              </a>
+            </span>
+          </button>
+        </div>
+      </section>
+      <section>
+        <h2>Still unsure? Step Inside and See What The Future Looks Like.</h2>
+        <button className="btn" type="button">
+          <span>
+            <a
+              href="/signup"
+              style={{ textDecoration: "none", color: "white" }}
+            >
+              Schedule a Demo
+              <ion-icon
+                name="arrow-forward-outline"
+                style={{ verticalAlign: "middle" }}
+              ></ion-icon>
+            </a>
+          </span>
+        </button>
+        <Mui.Divider variant="middle" />
+      </section>
+      <section style={{marginBottom:"20px"}}>
+        <div className="row footer-nav">
+          <ul>
+            <li>Product {">"}</li>
+            <li>Subscription Management</li>
+            <li>Recurring Billing and Invoicing</li>
+            <li>Recurring Payments</li>
+            <li>Accounting and Taxes</li>
+            <li>SaaS Reporting</li>
+            <li>Enterprise Billing</li>
+            <li> Integrations</li>
+          </ul>
+          <ul>
+            <li>Product {">"}</li>
+            <li>Subscription Management</li>
+            <li>Recurring Billing and Invoicing</li>
+            <li>Recurring Payments</li>
+            <li>Accounting and Taxes</li>
+            <li>SaaS Reporting</li>
+            <li>Enterprise Billing</li>
+            <li> Integrations</li>
+          </ul>
+          <ul>
+            <li>Product {">"}</li>
+            <li>Subscription Management</li>
+            <li>Recurring Billing and Invoicing</li>
+            <li>Recurring Payments</li>
+            <li>Accounting and Taxes</li>
+            <li>SaaS Reporting</li>
+            <li>Enterprise Billing</li>
+            <li> Integrations</li>
+          </ul>
+          <ul>
+            <li>Product {">"}</li>
+            <li>Subscription Management</li>
+            <li>Recurring Billing and Invoicing</li>
+            <li>Recurring Payments</li>
+            <li>Accounting and Taxes</li>
+            <li>SaaS Reporting</li>
+            <li>Enterprise Billing</li>
+            <li> Integrations</li>
+          </ul>
         </div>
       </section>
     </div>
